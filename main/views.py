cf2511ffdf64bfb72297475d2b34bf91cbd5ae0c
DaniyaRRa <d_raiymbekov@kbtu.kz>
2024-04-13 16:49:46 +0500

    13.04.24 New parser - population by each area

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import json
-from assets.scoring import init
+from django.db.models import Q
 from main.forms import StepForm
 from django.conf import settings
 from main.models import AreaScoring
@@ -12,10 +12,10 @@ def main_view(request):
     if request.method == 'POST' and form.is_valid():
         step = int(form.cleaned_data['Area_Size'])
 
-    data_new2 = AreaScoring.objects
-    data_new2 = data_new2.filter(step_id=step)
+    scoring = AreaScoring.objects
+    scoring = scoring.filter(step_id=step)
 
-    data_new2 = {('(' + str(row.latitude) + ', ' + str(row.longitude) + ')'): row.score for row in data_new2}
-    data_new2 = json.dumps(data_new2)
+    scoring = {('(' + str(row.latitude) + ', ' + str(row.longitude) + ')'): row.score for row in scoring}
+    scoring = json.dumps(scoring)
     step_dict = {1: 0.0025, 2: 0.005, 3: 0.01}
-    return render(request, 'map.html', {'form': form, 'data': data_new2, 'step': step_dict[step] or 0.01, 'google_map_api_key': settings.GOOGLE_MAP_API_KEY})
+    return render(request, 'map.html', {'form': form, 'data': scoring, 'step': step_dict[step] or 0.01, 'google_map_api_key': settings.GOOGLE_MAP_API_KEY})
